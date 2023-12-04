52f6655d16acb8cc1657d47cbe1ab45dd65319db
uzore <narchjosephs@gmail.com>
2023-12-04 08:40:36 +0100

    Add project files.

---
+++ MvcMovie/Models/SeedData.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcMovieContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "The Errand of Angels",
+                        ReleaseDate = DateTime.Parse("2008-2-12"),
+                        Genre = "Romantic Comedy",
+                        Rating = "R",
+                        Price = 7.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "Charly ",
+                        ReleaseDate = DateTime.Parse("2002-3-13"),
+                        Genre = "Comedy",
+                        Rating = "R",
+                        Price = 8.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "The Best Two Years",
+                        ReleaseDate = DateTime.Parse("2004-2-23"),
+                        Genre = "Comedy",
+                        Rating = "R",
+                        Price = 9.99M
+                    },
+                    new Movie
+                    {
+                        Title = "Mobsters and Mormons",
+                        ReleaseDate = DateTime.Parse("2005-4-15"),
+                        Genre = "Comedy",
+                        Rating = "R",
+                        Price = 3.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "The Other Side of Heaven",
+                        ReleaseDate = DateTime.Parse("2001-4-15"),
+                        Genre = "Western",
+                        Rating = "R",
+                        Price = 3.99M
+                    },
+                    new Movie
+                    {
+                        Title = "The Work and the Glory III: A House Divided",
+                        ReleaseDate = DateTime.Parse("2006-4-15"),
+                        Genre = "Western",
+                        Rating = "R",
+                        Price = 3.99M
+                    },
+                    new Movie
+                    {
+                        Title = "God's Army",
+                        ReleaseDate = DateTime.Parse("2000-4-15"),
+                        Genre = "Western",
+                        Rating = "R",
+                        Price = 3.99M
+                    },
+                    new Movie
+                    {
+                        Title = "The Singles 2nd Ward",
+                        ReleaseDate = DateTime.Parse("2007-4-15"),
+                        Genre = "Romance",
+                        Rating = "R",
+                        Price = 3.99M
+                    },
+                    new Movie
+                    {
+                        Title = "The R.M.",
+                        ReleaseDate = DateTime.Parse("2003-4-15"),
+                        Genre = "Comedy",
+                        Rating = "R",
+                        Price = 3.99M
+                    },
+                    new Movie
+                    {
+                        Title = "Meet the Mormons",
+                        ReleaseDate = DateTime.Parse("2014-4-15"),
+                        Genre = "Western",
+                        Rating = "R",
+                        Price = 3.99M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
